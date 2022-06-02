f77f636a02ad32640094b38a4f524ff0ce75099b
Matthew Bower <mttbwr91@gmail.com>
2022-06-01 21:49:01 -0700

    modal update

--- client/src/components/SubscribeButton.js
+++ client/src/components/SubscribeButton.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // npm's
-import { useForm } from 'react-hook-form';
-import Modal from 'react-modal';
-Modal.setAppElement('#root');
+import { useForm } from "react-hook-form";
+import Modal from "react-modal";
+Modal.setAppElement("#root");
 
 const SubscribeButton = () => {
   const [openModal, setOpenModal] = useState(false);
@@ -13,17 +13,17 @@ const SubscribeButton = () => {
   };
 
   const customStyles = {
-    content : {
-      background: 'linear-gradient(to right bottom, #4169E1, dodgerblue)',
-      bottom: 'auto',
+    content: {
+      background: "linear-gradient(to right bottom, #4169E1, dodgerblue)",
+      bottom: "auto",
       // display: 'flex',
-      left: '19%',
-      marginTop: '100px',
-      position: 'relative',
-      padding: '60px',
-      top: '10%',
-      textAlign: 'center',
-      width: '60%'
+      left: "19%",
+      // marginTop: "100px",
+      position: "relative",
+      padding: "60px",
+      top: "10%",
+      textAlign: "center",
+      width: "60%",
     },
     // overlay: {
     //   backgroundColor: '#fff'
@@ -35,9 +35,9 @@ const SubscribeButton = () => {
       <button className="subscribe-button" onClick={() => setOpenModal(true)}>
         <span>Subscribe!</span>
       </button>
-      <Modal 
+      <Modal
         className="subscribe-modal"
-        isOpen={openModal} 
+        isOpen={openModal}
         onRequestClose={() => setOpenModal(false)}
         style={customStyles}
       >
@@ -45,40 +45,40 @@ const SubscribeButton = () => {
           {/* <i className="fas fa-times-circle"></i> */}
           {/* <h1>&times;</h1> */}
           {/* <h4>Exclusive deals just for you!</h4> */}
-          
+
           <form className="subscribe-form" onSubmit={handleSubmit(onSubmit)}>
-            <input 
-              type="text" 
-              name="name" 
-              ref={register({ required: true })} 
-              placeholder='Name' 
-              style={{width: '50%'}}
-            />
-            {errors.name && 'Name is required.'}
-            <input 
-              type='email'
-              name="email" 
-              ref={register({ required: true })} 
-              placeholder='Email' 
-              style={{width: '50%'}}
-            />
-            {errors.email && 'Email is required.'}
-            <input 
+            <input
               type="text"
-              name="phone" 
-              ref={register({ required: true })} 
-              placeholder='Phone'
-              style={{width: '50%'}}
+              name="name"
+              ref={register({ required: true })}
+              placeholder="Name"
+              style={{ width: "50%" }}
             />
-            {errors.phone && 'Name is required.'}
-            <button type='submit'>Submit</button>
+            {errors.name && "Name is required."}
+            <input
+              type="email"
+              name="email"
+              ref={register({ required: true })}
+              placeholder="Email"
+              style={{ width: "50%" }}
+            />
+            {errors.email && "Email is required."}
+            <input
+              type="text"
+              name="phone"
+              ref={register({ required: true })}
+              placeholder="Phone"
+              style={{ width: "50%" }}
+            />
+            {errors.phone && "Name is required."}
+            <button type="submit">Submit</button>
           </form>
         </div>
-        
+
         {/* <button onClick={() => setOpenModal(false)}>Close</button> */}
       </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default SubscribeButton
+export default SubscribeButton;
